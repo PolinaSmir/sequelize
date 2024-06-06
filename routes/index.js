096f18bbf73c1a4a5e783c5d32853cc20859cb39
Polina <smivasilisa@gmail.com>
2024-06-06 19:47:32 +0200

    Add user validate and error handler

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const UserController = require("../controllers/User.controller");
 const TaskController = require("../controllers/Task.controller");
-const { getUserInstance } = require("../middlewares/user.mw");
+const { getUserInstance, validateUser } = require("../middlewares/user.mw");
 
 const router = Router();
 
-router.post("/user", UserController.createUser);
+router.post("/user", validateUser, UserController.createUser);
 router.get("/users", UserController.findAll);
 router.get("/user/:userId", getUserInstance, UserController.findByPk);
 router.delete("/user/:userId", UserController.deleteByPk);
