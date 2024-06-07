e4b8b47a450a2093500d1122bba59f1a0e6ad845
Polina <smivasilisa@gmail.com>
2024-06-07 20:54:05 +0200

    Get group with members

--- routes/groupRouter.js
+++ routes/groupRouter.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const GroupController = require("../controllers/Group.controller");
 const { getUserInstance } = require("../middlewares/user.mw");
+const pagination = require("../middlewares/pagination.mw");
 
 const groupRouter = Router();
 
@@ -8,5 +9,6 @@ groupRouter.post("/", GroupController.createGroup);
 groupRouter.put("/:userId/:groupId", getUserInstance, GroupController.addUserToGroup);
 groupRouter.get("/:userId", getUserInstance, GroupController.getUserGroups);
 groupRouter.delete("/:userId/:groupId", getUserInstance, GroupController.deleteUserFromGroup);
+groupRouter.get("/:groupId/members", pagination, GroupController.getGroupWithMembers);
 
 module.exports = groupRouter;
