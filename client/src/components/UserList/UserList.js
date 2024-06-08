debb2580951aa7888c384bf2817854a4e1251b40
Polina <smivasilisa@gmail.com>
2024-06-08 20:01:20 +0200

    Get user done modal window

--- client/src/components/UserList/UserList.js
+++ client/src/components/UserList/UserList.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from "react";
 import { getUsers } from "../../api";
 import UserCard from "./UserCard";
 import "./style.css";
+import UserCardModal from "./UserCardModal";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const loadUsers = (pageNumber) => {
     getUsers(pageNumber)
@@ -23,7 +26,16 @@ const UserList = () => {
   };
 
   const renderUsers = () => {
-    return users.map((user) => <UserCard user={user} key={user.id} />);
+    return users.map((user) => (
+      <UserCard
+        user={user}
+        key={user.id}
+        onClick={() => {
+          setSelectedUser(user);
+          setIsModalOpen(true);
+        }}
+      />
+    ));
   };
 
   const prevBtnHandler = () => {
@@ -46,6 +58,8 @@ const UserList = () => {
 
       {isLoading && <h2 className="loading">Loading....</h2>}
       <section className="card-container">{users.length > 0 ? renderUsers() : <h2 className="error">Users not found</h2>}</section>
+
+      <UserCardModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} selectedUser={selectedUser} />
 
       <div>
         <button onClick={prevBtnHandler} disabled={page === 1}>
