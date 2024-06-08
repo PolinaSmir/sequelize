9effaa430bd270d894f3d9bcdaf22445eb4744f3
Polina <smivasilisa@gmail.com>
2024-06-08 18:28:45 +0200

    Get users request - done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,12 @@
+import UserList from "./UserList/UserList";
+
 function App() {
-  return <h1>Hello front-end!</h1>;
+  return (
+    <>
+      <h1>Hello front-end!</h1>
+      <UserList />
+    </>
+  );
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = require("./routes/index");
 const { errorHandler } = require("./errorHandler");
 const { STATIC_PATH } = require("./config/path.config");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static(STATIC_PATH));
