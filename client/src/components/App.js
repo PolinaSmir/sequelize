7eeeae0fac3e545e9f844a68926fa65d1e86678f
Polina <smivasilisa@gmail.com>
2024-06-08 19:25:12 +0200

    Get users done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,6 @@ import UserList from "./UserList/UserList";
 function App() {
   return (
     <>
-      <h1>Hello front-end!</h1>
       <UserList />
     </>
   );
--- client/src/components/UserList/UserList.js
+++ client/src/components/UserList/UserList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getUsers } from "../../api";
+import UserCard from "./UserCard";
+import "./style.css";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
@@ -20,11 +22,41 @@ const UserList = () => {
       });
   };
 
+  const renderUsers = () => {
+    return users.map((user) => <UserCard user={user} key={user.id} />);
+  };
+
+  const prevBtnHandler = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const nextBtnHandler = () => {
+    setPage(page + 1);
+  };
+
   useEffect(() => {
     loadUsers(page);
   }, [page]);
 
-  return <div></div>;
+  return (
+    <>
+      <h1>User List</h1>
+
+      {isLoading && <h2 className="loading">Loading....</h2>}
+      <section className="card-container">{users.length > 0 ? renderUsers() : <h2 className="error">Users not found</h2>}</section>
+
+      <div>
+        <button onClick={prevBtnHandler} disabled={page === 1}>
+          Previous page
+        </button>
+        <button onClick={nextBtnHandler} disabled={users.length < 5}>
+          Next page
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default UserList;
